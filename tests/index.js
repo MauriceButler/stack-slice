e7138c0ed46d6530827cf59c9dac40e6128be4f6
mauricebutler <maurice.butler@gmail.com>
2015-10-13 17:27:00 +1000

    initial commit

---
+++ tests/index.js
@@ -0,0 +1,44 @@
+var test = require('tape'),
+    path = require('path'),
+    stackSlice = require('../');
+
+test('no touchy', function(t){
+    t.plan(1);
+
+    var testError = new Error('BANG!!!!'),
+        originalStack = testError.stack;
+
+    stackSlice(testError, 'something that wont match');
+
+    t.equal(originalStack, testError.stack, 'stack was not modified');
+});
+
+test('exact match', function(t){
+    t.plan(1);
+
+    var testError = new Error('BANG!!!!'),
+        originalStack = testError.stack.split('\n');
+
+    originalStack.splice(1,1);
+
+    var expectedStack = originalStack.join('\n');
+
+    stackSlice(testError, __filename);
+
+    t.equal(testError.stack, expectedStack, 'stack was modified correctly');
+});
+
+test('partial match', function(t){
+    t.plan(1);
+
+    var testError = new Error('BANG!!!!'),
+        originalStack = testError.stack.split('\n'),
+        expectedStack = [];
+
+    expectedStack.push(originalStack.shift());
+    expectedStack.push(originalStack.pop());
+
+    stackSlice(testError, path.resolve(__dirname, '..'), true);
+
+    t.equal(testError.stack, expectedStack.join('\n'), 'stack was modified correctly');
+});
